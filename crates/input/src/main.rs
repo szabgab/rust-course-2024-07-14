6357f3726c8d6958578f3bae29bc0f3519681700
Gabor Szabo <gabor@szabgab.com>
2024-07-14 12:02:01 +0300

    autocommit

--- crates/input/src/main.rs
+++ crates/input/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    dbg!(args);
-    println!("{}", &args[1]);
+    println!("{:?}", args);
+    println!("{}", args[1]);
+    println!
     // let answer = "42\n";
     // let answer = answer.trim().parse::<i32>().unwrap();
     // println!("{answer}");
