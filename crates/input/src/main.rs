ffc6a278dab0f29c3f53fad2a782ac7af63be62a
Gabor Szabo <gabor@szabgab.com>
2024-07-14 13:48:01 +0300

    autocommit

--- crates/input/src/main.rs
+++ crates/input/src/main.rs
@@ -6,4 +6,8 @@ fn main() {
     // let answer = "42\n";
     // let answer = answer.trim().parse::<i32>().unwrap();
     // println!("{answer}");
+
+    let x = 7889;
+    let y:i8 = 10;
+    let z = x + y;
 }
