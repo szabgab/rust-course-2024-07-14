2e70adaf72d7afa2f5778bd1891712e7cfa52ffb
Gabor Szabo <gabor@szabgab.com>
2024-07-15 10:36:01 +0300

    autocommit

--- crates/owners/src/main.rs
+++ crates/owners/src/main.rs
@@ -1,23 +1,24 @@
 fn main() {
     let text = "Foo Bar";
-    
+    let other = &text[0..3];
+    println!("{other}");
 
-    let text = String::from("Foo Bar\n");
-    println!("{text}");
-
-    // ptr  
-    // len
-    // capacity
-    println!("{:p} {:?} {}", &text, text.as_ptr(), text.len());
-
-    let short = text.trim_end();
-    println!("{:p} {:?} {}", &short, short.as_ptr(), short.len());
-    println!("{:p} {:?} {}", &text, text.as_ptr(), text.len());
-    
-    // let other = &text;
-    // println!("{other}");
-
-    // // text = other;
+    // let text = String::from("Foo Bar\n");
     // println!("{text}");
+
+    // // ptr  
+    // // len
+    // // capacity
+    // println!("{:p} {:?} {}", &text, text.as_ptr(), text.len());
+
+    // let short = text.trim_end();
+    // println!("{:p} {:?} {}", &short, short.as_ptr(), short.len());
+    // println!("{:p} {:?} {}", &text, text.as_ptr(), text.len());
+    
+    // // let other = &text;
+    // // println!("{other}");
+
+    // // // text = other;
+    // // println!("{text}");
 
 }
