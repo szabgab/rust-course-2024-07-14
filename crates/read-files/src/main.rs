89b68bc02cae68b3bdecdad03d1637c5e981bedb
Gabor Szabo <gabor@szabgab.com>
2024-07-15 13:36:01 +0300

    autocommit

--- crates/read-files/src/main.rs
+++ crates/read-files/src/main.rs
@@ -2,17 +2,31 @@ fn main() {
     let args = std::env::args().collect::<Vec<_>>();    
     println!("{args:?}");
 
+    fun_name(args);
+}
+
+fn fun_name(args: Vec<String>) {
     for filename in &args[1..] {
         println!("{filename}");
         // let content = std::fs::read_to_string(filename).unwrap();
         // let content = std::fs::read_to_string(filename).expect("blblbla");
         // println!("{:?}", content);
+        // match std::fs::read_to_string(filename) {
+        //     Ok(val) => println!("{val}"),
+        //     Err(err) => {
+        //         eprintln!("{err}");
+        //         //std::process::exit(1);
+        //         return;
+        //     }
+        // }
+
         match std::fs::read_to_string(filename) {
             Ok(val) => println!("{val}"),
             Err(err) => {
                 eprintln!("{err}");
-                std::process::exit(1);
+                return;
             }
         }
+
     }
 }
