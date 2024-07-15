2d1ebd39d159a1c6a4ae6660f157ad1ccf919b36
Gabor Szabo <gabor@szabgab.com>
2024-07-15 10:50:01 +0300

    autocommit

--- crates/memory/src/main.rs
+++ crates/memory/src/main.rs
@@ -6,4 +6,12 @@ fn main() {
     println!("{} {} {:p} {:?}", text.len(), text.capacity(), &text, text.as_ptr());
     text.push('😁');
     println!("{} {} {:p} {:?}", text.len(), text.capacity(), &text, text.as_ptr());
+
+    // let hi = "";
+    text.push_str("qqrq");
+    println!("{} {} {:p} {:?}", text.len(), text.capacity(), &text, text.as_ptr());
+
+    text.push_str("123456789012345678");
+    println!("{} {} {:p} {:?}", text.len(), text.capacity(), &text, text.as_ptr());
+
 }
