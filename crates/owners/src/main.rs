92f7de3689cac131b95f8bfcceec39beae6d1186
Gabor Szabo <gabor@szabgab.com>
2024-07-15 10:18:01 +0300

    autocommit

--- crates/owners/src/main.rs
+++ crates/owners/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    let mut text = String::from("hello");
+    let text = String::from("Foo Bar\n");
     println!("{text}");
+
+    //let new_name = name.trim_end().to_owned();
+    println!("{:p}", &text);
 
     let other = &text;
     println!("{other}");
