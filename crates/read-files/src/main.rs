85762544b9cc92a6042933e0e04c5dcb260f9c04
Gabor Szabo <gabor@szabgab.com>
2024-07-15 13:24:01 +0300

    autocommit

--- crates/read-files/src/main.rs
+++ crates/read-files/src/main.rs
@@ -2,5 +2,8 @@ fn main() {
     let args = std::env::args().collect::<Vec<_>>();    
     println!("{args:?}");
 
-    for file in args
+    for filename in &args[1..] {
+        println!("{filename}");
+        std::
+    }
 }
