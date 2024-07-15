73542e8c46388ae170a34c5172a17f5cf13addf1
Gabor Szabo <gabor@szabgab.com>
2024-07-15 10:40:01 +0300

    autocommit

--- crates/owners/src/main.rs
+++ crates/owners/src/main.rs
@@ -1,25 +1,28 @@
 fn main() {
-    let text = "Foo Bar";
-    // let other = &text[0..3];
-    let other = &text[4..];
-    println!("{other}");
+    // let text = "Foo Bar";
+    // // let other = &text[0..3];
+    // let other = &text[4..];
+    // println!("{other}");
 
-    // let text = String::from("Foo Bar\n");
-    // println!("{text}");
+    let text = String::from("Foo Bar\n");
+    println!("{text}");
+    let fname = &text[0..3];
+    println!("{fname}");
 
-    // // ptr  
-    // // len
-    // // capacity
-    // println!("{:p} {:?} {}", &text, text.as_ptr(), text.len());
 
-    // let short = text.trim_end();
-    // println!("{:p} {:?} {}", &short, short.as_ptr(), short.len());
-    // println!("{:p} {:?} {}", &text, text.as_ptr(), text.len());
+    // ptr  
+    // len
+    // capacity
+    println!("{:p} {:?} {}", &text, text.as_ptr(), text.len());
+
+    let short = text.trim_end();
+    println!("{:p} {:?} {}", &short, short.as_ptr(), short.len());
+    println!("{:p} {:?} {}", &text, text.as_ptr(), text.len());
     
-    // // let other = &text;
-    // // println!("{other}");
+    // let other = &text;
+    // println!("{other}");
 
-    // // // text = other;
-    // // println!("{text}");
+    // // text = other;
+    // println!("{text}");
 
 }
