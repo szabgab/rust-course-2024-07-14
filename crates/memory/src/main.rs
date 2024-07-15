c001e7adacf54c478ebc7bacb16985ab875997d8
Gabor Szabo <gabor@szabgab.com>
2024-07-15 10:48:01 +0300

    autocommit

--- crates/memory/src/main.rs
+++ crates/memory/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
     let mut text = String::new();
 
-    println!("{} {} {:p} {:?}", text.len(), text.capacity(), &text, text.as_ptr());]\
+    println!("{} {} {:p} {:?}", text.len(), text.capacity(), &text, text.as_ptr());
+    text.push('a');
+    println!("{} {} {:p} {:?}", text.len(), text.capacity(), &text, text.as_ptr());
+    text.push('😁');
+    println!("{} {} {:p} {:?}", text.len(), text.capacity(), &text, text.as_ptr());
 }
