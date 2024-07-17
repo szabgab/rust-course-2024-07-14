ede169ed58dc5388ae231e2c75eb997caac59df0
Gabor Szabo <gabor@szabgab.com>
2024-07-17 09:20:01 +0300

    autocommit

---
+++ crates/strings/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let text = "שלום עולם";
+    println!("{}", text.chars().nth(2).unwrap());
+
+
+    
+    // let mut text = String::from("The black cat climbed the green tree!");
+    // let x = &text[0..3];
+    // println!("{}", x);
+    // let y = text.replace("The", "qqq");
+
+    // println!("{}", text);
+    // println!("{}", y);
+    // println!("'{}'", &text[4..4]);
+    // println!("'{}'", &text[4..=4]);
+    // println!("'{}'", &text[4..5]);
+    // println!("'{}'", &text[4..9]);
+    // println!("'{}'", &text[4..=8]);
+    // println!("'{}'", &text[4..]);
+    // println!("'{}'", &text[4..text.len()]);
+    // println!("'{}'", &text[4..text.len()-1]);
+    // println!("'{}'", &text[..4]);
+
+    // println!("'{}'", &text[4..40]);  // thread 'main' panicked at 'byte index 40 is out of bounds
+}
