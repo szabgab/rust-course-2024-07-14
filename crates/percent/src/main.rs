05e0191a4dd2b52ec91a8b757d1abef7390a5062
Gabor Szabo <gabor@szabgab.com>
2024-07-17 09:54:01 +0300

    autocommit

--- crates/percent/src/main.rs
+++ crates/percent/src/main.rs
@@ -3,7 +3,17 @@ struct Foo {
 }
 
 fn main() {
-    let a = Foo { name: "abc" };
-    
+    let a = Foo {
+        name: String::from("abc"),
+    };
+    let b = Foo {
+        name: String::from("abc"),
+    };
+
+    match a {
+        b => println!("matched"),
+        _ => println!("not matched"),
+    }
+
     println!("Hello, world!");
 }
