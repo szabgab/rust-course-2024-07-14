d1279efd0659c423d992a34b91954f4fa5d613b0
Gabor Szabo <gabor@szabgab.com>
2024-07-17 09:48:01 +0300

    autocommit

--- crates/string-change/src/main.rs
+++ crates/string-change/src/main.rs
@@ -1,11 +1,26 @@
+macro_rules! prt {
+    ($var:expr) => {
+        println!(
+            "{:2} {:2} {:p} {:15?} '{}'",
+            $var.len(),
+            $var.capacity(),
+            &$var,
+            $var.as_ptr(),
+            $var
+        );
+    };
+}
+
 fn main() {
     let text = String::from("Foo");
     println!("Main: {text}");
     display(&text);
-    
+    prt!(text);
+
     let new = change(text);
     //println!("Main: {text}");
     println!("New: {new}");
+    prt!(new);
     //display(&text);
 }
 
