244da45c22bcfd776b63f6227fa82b910913133b
Gabor Szabo <gabor@szabgab.com>
2024-07-17 09:46:01 +0300

    autocommit

--- crates/string-change/src/main.rs
+++ crates/string-change/src/main.rs
@@ -1,17 +1,20 @@
 fn main() {
-    let mut text = String::from("Foo");
+    let text = String::from("Foo");
     println!("Main: {text}");
     display(&text);
+    
     let new = change(text);
-    println!("Main: {text}");
-    display(&text);
+    //println!("Main: {text}");
+    println!("New: {new}");
+    //display(&text);
 }
 
 fn display(txt: &str) {
     println!("Display: {txt}")
 }
 
-fn change(mut txt: String) {
+fn change(mut txt: String) -> String {
     //*txt = String::from("Bar");
     txt.push_str(" Bar");
+    txt
 }
