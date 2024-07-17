4b5feeaf125655abd1d7c76a5c75fa1ad08dd712
Gabor Szabo <gabor@szabgab.com>
2024-07-17 14:30:01 +0300

    autocommit

--- crates/generic-functions/src/main.rs
+++ crates/generic-functions/src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
+    let a = 7;
+    let b = 8;
+    println!("{}", add(a, b));
 
-    println!("Hello, world!");
+    let c = 7_i8;
+    let d = 29_i8;
+    println!("{}", add(c, d));
 }
 
 fn add(x: i32, y: i32) -> i32 {
-    
+    x + y
 }
